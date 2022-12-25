a009f68aaca1239e4827c5b1c26b03869a55425d
Kelvin Li <li.kelvin.kl@gmail.com>
2022-12-24 22:39:19 -0500

    day10

---
+++ 2022/day10/advent9.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 10 00:05:05 2022
+
+@author: kelvinli
+"""
+
+file1 = open('advent9.txt', 'r')
+Lines = file1.readlines()
+
+greater = 1
+cycle = 0
+
+for i in Lines:
+    if i[0:4] == "noop":
+        cycle += 1
+    
+    elif i[0:4] == "addx":
+        
+        
+        cycle += 1
+        print(cycle, greater)
+        greater += int(i[5:])
+        cycle += 1
+        
+        
+        
+
+
+    print(cycle, greater)
+    
+ 
+    
+print(20* 21 + 60 *23    +100 * 25  +140 * 14 +180* 5 + 220*21)
